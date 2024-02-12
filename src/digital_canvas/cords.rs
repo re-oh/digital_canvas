c02674376db66779b006276539b3e958acbdcade
re-oh <dabrowskilucas2@gmail.com>
2024-02-12 11:56:57 +0100

    optimized cords.rs, added delta method to Cords2D. Added constructor and to_tuple to Dimensions.

--- src/digital_canvas/cords.rs
+++ src/digital_canvas/cords.rs
@@ -4,20 +4,28 @@ pub struct Cords2D {
 }
 
 impl Cords2D {
-  pub fn new( x: u32, y: u32 ) -> Self {
-    Self {x, y}
+  pub fn new(x: u32, y: u32) -> Self {
+      Cords2D { x, y }
   }
 
-  pub fn to_tuple ( &self ) -> ( u32, u32 ) {
-    ( self.x, self.y )
+  pub fn to_tuple(&self) -> (u32, u32) {
+      (self.x, self.y)
   }
 
-  pub fn distance(&self, cords: &Cords2D) -> u32{
-    ((((( self.x as i32 - cords.x as i32  ) as f32).powi(2)) + ((( self.y as i32 - cords.y as i32 ) as f32).powi(2))).sqrt().round()) as u32
+  pub fn distance(&self, other: &Cords2D) -> u32 {
+      (((((self.x as i32 - other.x as i32) as f32).powi(2))
+          + (((self.y as i32 - other.y as i32) as f32).powi(2)))
+          .sqrt()
+          .round()) as u32
   }
 
-  pub fn centroid(&self, cords: &Cords2D) -> Cords2D {
-    Cords2D::new((self.x + cords.x) /  2, (self.y + cords.y) / 2)
+  pub fn centroid(&self, other: &Cords2D) -> Cords2D {
+      Cords2D::new((self.x + other.x) / 2, (self.y + other.y) / 2)
   }
 
+  pub fn delta(&self, other: &Cords2D) -> (i32, i32) {
+    let dx = other.x as i32 - self.x as i32;
+    let dy = other.y as i32 - self.y as i32;
+    (dx, dy)
+}
 }
--- src/digital_canvas/dimensions.rs
+++ src/digital_canvas/dimensions.rs
@@ -3,3 +3,17 @@ pub struct Dimensions {
   pub width: u32,
   pub height: u32,
 }
+
+impl Dimensions {
+  fn new(width: u32, height: u32) -> Self {
+    Self {
+      width,
+      height
+    }
+  }
+
+  fn to_tuple(&self) -> (u32, u32) {
+    (self.width, self.height)
+  }
+
+}
